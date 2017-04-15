fe74988afa28d2774e2c0083a6245b3cc63b5159
JEFFREY <abc@email.com>
2017-04-15 05:19:07 +0000

    deploy

--- routes/index.js
+++ routes/index.js
@@ -17,55 +17,60 @@ async.series({
   },
   get_user_list: function(callback) {
     console.log('get_user_list');
-    db.collection('user_list').find().toArray(function (err, result) {
-      if (err) throw err
-      callback(null, result);
-    })
+    callback(null, null);
   },
   get_user_img: function(callback) {
-    var username = 'alkarus'
-    request('https://www.instagram.com/'+username+'/media/', function (err, response, body) {
+    db.collection('user_list').find().toArray(function (err, results) {
       if (err) throw err
-      if(response.statusCode === 200){
-        var img_arr = []
-        body = JSON.parse(body)
-        body.items.forEach(function (item) {
-          if(item.type === "image"){
-            var img_obj = {}
-            img_obj.id = item.id
-            img_obj.username = username
-            img_obj.created_time = item.created_time
-            img_obj.likes = item.likes.count
-            img_obj.caption = item.caption
-            img_obj.images = item.images
-            img_arr.push(img_obj)
-          }
-        })
-        if(img_arr.length > 0){
-          var query = {
-            username: username
-          };
-          db.collection('images').remove(query)
-          .then(()=>{
-            img_arr.forEach(function (img) {
-              db.collection('images').insertOne( img, function(err, result) {
-                if (err) throw err
-              });
+      
+      results.forEach((result)=>{
+        var username = result.username
+        
+        request('https://www.instagram.com/'+username+'/media/', function (err, response, body) {
+          if (err) throw err
+          if(response.statusCode === 200){
+            var img_arr = []
+            body = JSON.parse(body)
+            body.items.forEach(function (item) {
+              if(item.type === "image"){
+                var img_obj = {}
+                img_obj.id = item.id
+                img_obj.username = username
+                img_obj.created_time = item.created_time
+                img_obj.likes = item.likes.count
+                img_obj.caption = item.caption
+                img_obj.images = item.images
+                img_arr.push(img_obj)
+              }
             })
-          })
-          .then(()=>{
-            console.log("Inserted "+username+" into the images collection.");
+            if(img_arr.length > 0){
+              var query = {
+                username: username
+              };
+              db.collection('images').remove(query)
+              .then(()=>{
+                img_arr.forEach(function (img) {
+                  db.collection('images').insertOne( img, function(err, result) {
+                    if (err) throw err
+                  });
+                })
+              })
+              .then(()=>{
+                console.log("Inserted "+username+" into the images collection.");
+                callback(null, null);
+              })
+              .catch((err)=>{
+                if (err) throw err
+              })
+            }
+          }else{
+            console.log('statusCode:', response && response.statusCode);
             callback(null, null);
-          })
-          .catch((err)=>{
-            if (err) throw err
-          })
-        }
-      }else{
-        console.log('statusCode:', response && response.statusCode);
-        callback(null, null);
-      }
-    });
+          }
+        });
+        
+      })
+    })
   }
 }, function(err, results) {
   if (err) throw err
