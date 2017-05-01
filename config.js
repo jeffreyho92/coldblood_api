8ff77bf907550020c2b0803f8b72e070020e0b03
JEFFREY <abc@email.com>
2017-05-01 09:42:41 +0000

    deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+//config.mongodb_url = 'mongodb://admin:admin@ds155150.mlab.com:55150/lawa';
+config.mongodb_url = 'mongodb://admin:admin@localhost:27017/lawa';
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../config.js');
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
+var arr = [];
+
+MongoClient.connect(config.mongodb_url, function (err, database) {
+    if (err) throw err
+    db = database;
+    console.log("Connected to database");
+    
+    db.collection('user_list').find().toArray(function (err, result) {
+      if (err) throw err
+      result.forEach(function (res) {
+          var obj = {username: res.username, category: res.category };
+          arr.push(obj)
+      });
+    })
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(JSON.stringify(arr));
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
